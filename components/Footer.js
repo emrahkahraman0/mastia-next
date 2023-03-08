9503fe1de8d480cc6c6110e975bbd502006c018a
emrahkahraman0 <oempire34@gmail.com>
2023-03-08 22:27:16 +0300

    devamke

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 function Footer() {
   return (
     <>
       <div id="footer">
         <div className="container">
-          <div className="footer">
+          <div className="footer row row-cols-xl-4 row-cols-lg-4 row-cols-md-2 row-cols-sm-2">
             <div className="footer_item">
               <h6>About Us</h6>
               <p>
@@ -15,10 +17,44 @@ function Footer() {
             {/*footer_item*/}
             <div className="footer_item">
               <h6>Related Links</h6>
+              <ul>
+                <li>
+                  <Link href="/" Official Site></Link>
+                </li>
+                <li>
+                  <Link href="/" CreativeMarket profile></Link>
+                </li>
+                <li>
+                  <Link href="/" Materialzer page on CM></Link>
+                </li>
+                <li>
+                  <Link href="/" Electronic WP Theme></Link>
+                </li>
+                <li>
+                  <Link href="/" Was favvvv></Link>
+                </li>
+              </ul>
             </div>
             {/*footer_item*/}
             <div className="footer_item">
               <h6>News & Mediaout</h6>
+              <ul>
+                <li>
+                  <Link href="/" Official Site></Link>
+                </li>
+                <li>
+                  <Link href="/" CreativeMarket profile></Link>
+                </li>
+                <li>
+                  <Link href="/" Materialzer page on CM></Link>
+                </li>
+                <li>
+                  <Link href="/" Electronic WP Theme></Link>
+                </li>
+                <li>
+                  <Link href="/" Was favvvv></Link>
+                </li>
+              </ul>
             </div>
             {/*footer_item*/}
             <div className="footer_item">
@@ -35,6 +71,14 @@ function Footer() {
         {/*container*/}
       </div>
       {/*footer#*/}
+
+      <div id="copyright">
+        <div className="container text">
+          Made with  by Revolthemes.net All Rights Reserved
+        </div>
+        {/*ctext*/}
+      </div>
+      {/*copyright#*/}
     </>
   );
 }
