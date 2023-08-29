c9c03a3dd7974aee606fe58983ecb539e8370423
emrahkahraman0 <oempire34@gmail.com>
2023-08-29 23:32:21 +0300

    update-10

--- src/app/blog/[...slug]/page.js
+++ src/app/blog/[...slug]/page.js
@@ -1,11 +1,43 @@
-import React from "react";
 import Pages from "@/src/templates/Pages";
+import Image from "next/image";
+import Blogİmg from "public/400x400.png";
 
-function BlogDetail() {
+//Async
+async function getPost(id) {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  return response.json();
+  if (!response.ok) {
+    throw new Error("failed to fetch posts");
+  }
+}
+
+async function BlogDetail({ params: { id } }) {
+  const post = await getPost(id);
+
   return (
     <>
       <Pages />
-      <div>BlogDetail</div>
+      <div id="blog_detail">
+        <div className="container">
+          <div className="blog_detail row row-cols-xl-2 row-cols-lg-2 row-cols-md-2 row-cols-sm-2">
+            <div className="blog_detail_image">
+              <Image className="img_fluid" src={Blogİmg} alt="Blog İmage" />
+            </div>
+            {/*blog_detail_image*/}
+            <div className="blog_detail_text">
+              <h6>{post.title}</h6>
+              <p>{post.body}</p>
+            </div>
+            {/*blog_detail_text">
+              <Image className="img_fluid*/}
+          </div>
+          {/*blog_detail*/}
+        </div>
+        {/*container*/}
+      </div>
+      {/*blog_detail#*/}
     </>
   );
 }
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,5 +1,7 @@
-import React from "react";
 import Pages from "@/src/templates/Pages";
+import Link from "next/link";
+import Image from "next/image";
+import Blogİmg from "public/400x400.png";
 
 function Blog() {
   return (
@@ -7,9 +9,24 @@ function Blog() {
       <Pages />
       <div id="blog">
         <div className="container">
-          <div className="blog row row-cols-xl-1 row-cols-lg-1 row-cols-md-1 row-cols-sm-1">
-            <div className="blog_item"></div>
-            {/*blog_posts*/}
+          <div className="blog row row-cols-xl-3 row-cols-lg-3 row-cols-md-2 row-cols-sm-2">
+            <div className="blog_item">
+              <Image className="img_fluid" src={Blogİmg} alt="Blog İmage" />
+              <h6>
+                <Link href="/">Blog One</Link>
+              </h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio nobis neque, corporis minima nisi, nostrum, impedit
+                ullam laboriosam laborum deserunt quo doloribus officia
+                voluptatem dignissimos exercitationem in possimus maiores
+                incidunt.
+              </p>
+              <Link className="read_more" href="/">
+                read more
+              </Link>
+            </div>
+            {/*blog_item*/}
           </div>
           {/*blog*/}
         </div>
